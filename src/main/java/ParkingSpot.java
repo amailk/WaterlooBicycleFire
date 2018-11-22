0e13dcc6eb951aeed451938811f49ec8ce3de410
Ama Liyanage <liyaniama@gmail.com>
2018-11-21 19:46:24 -0500

    update gitignore and parkingspot/parser java.

--- src/main/java/ParkingSpot.java
+++ src/main/java/ParkingSpot.java
@@ -1,2 +1,42 @@
+import com.google.cloud.firestore.GeoPoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingSpot {
+    private final String type;
+    private final float longitude;
+    private final float latitude;
+    private final String description;
+    private final String address;
+    private final int capacity;
+
+
+    public ParkingSpot(String type, float longitude, float latitude, String description, String address, int capacity) {
+        this.type = type;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.description = description;
+        this.address = address;
+        this.capacity = capacity;
+
+    }
+    //testing fetching the data from firebase.
+
+    @Override
+    public String toString() {
+        String parkingSpotInfo = String.format("type: %s, ln: %f, lat: %f, desc: %s, add: %s, cap: %d", type, longitude,latitude, description, address, capacity);
+        return parkingSpotInfo;
+    }
+    // creating a map(like a dictionary in Python) thats maps from a name(string) to an object.
+    public Map<String, Object> toMap() {
+        Map<String,Object> data = new HashMap<>();
+        data.put("type", this.type);
+        data.put("location", new GeoPoint(latitude, longitude));
+        data.put("description", this.description);
+        data.put("address", this.address);
+        data.put("capacity", this.capacity);
+
+        return data;
+    }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,70 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
+    public static void main(String[] args) {
+        String csvFile = "Bicycle_Parking.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        BufferedReader br = null;
+
+        // list of type ParkingSpot = new array list.
+        List<ParkingSpot> parkingSpotList = new ArrayList<>();
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+
+                // use comma as seperator
+                String[] values = line.split(cvsSplitBy);
+
+                System.out.println(values[0].trim());
+
+                // attributes of parking spot object.
+                String type = values[3].trim();
+                float longitude = Float.parseFloat(values[8].trim());
+                float latitude = Float.parseFloat(values[9].trim());
+                String description = values[5].trim();
+                String address = values[6].trim();
+                int capacity = 0;
+                try {
+                    capacity = Integer.parseInt(values[7].trim());
+                } catch (NumberFormatException e){
+
+                }
+
+                ParkingSpot parkingSpot = new ParkingSpot(type, longitude, latitude, description, address, capacity);
+
+                parkingSpotList.add(parkingSpot);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+//        testing
+//        System.out.println(parkingSpotList.get(0));
+//        System.out.println(parkingSpotList.get(1));
+//        System.out.println(parkingSpotList.get(10));
+
+        FirebaseService firebaseService = new FirebaseService();
+        firebaseService.uploadData(parkingSpotList);
+    }
+
 }
